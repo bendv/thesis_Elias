da69d901b65e1f1e94e295334a3e73038fd465c2
Ben <devries.br@gmail.com>
2013-11-29 11:01:37 +0100

    Create intersectExtent.R
    Function to determine the common extent of multiple raster layers. Input should be a list object containing raster or sp objects (ie. any object with an extent attribute).

---
+++ intersectExtent.R
@@ -0,0 +1,32 @@
+intersectExtent <- function(x){
+  # output an extent object representing the union of all extents
+  # arguments:
+  # x - either a list of rasterLayers, sp objects, or Extent objects
+  
+  # check if input is a list
+  if(!is.list(x)){
+    stop("x should be a list of RasterLayers, sp objects or Extent objects.\n")
+  }
+  
+  # check object classes in list
+  classes <- unique(unlist(sapply(x, class)))
+  if(length(classes) > 1 | !classes %in% c("RasterLayer", "RasterBrick", "RasterStack", "Extent", "SpatialPolygons", "SpatialPolygonsDataFrame")){
+    stop("x should be a list of RasterLayers, sp objects or Extent objects.\n")
+  }
+  
+  # extract extents
+  if(classes=="Extent"){
+    e <- x
+  } else {
+    e <- lapply(x, extent)
+  }
+  
+  # define union Extent
+  isectxmin <- max(unlist(lapply(e, xmin)))
+  isectxmax <- min(unlist(lapply(e, xmax)))
+  isectymin <- max(unlist(lapply(e, ymin)))
+  isectymax <- min(unlist(lapply(e, ymax)))
+  isecte <- extent(c(isectxmin, isectxmax, isectymin, isectymax))
+  
+  return(isecte)
+}
